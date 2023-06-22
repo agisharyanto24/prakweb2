afe985bcca09aa0787311625fc5b1b511076edf4
agisharyanto24 <agisharyanto100@gmail.com>
2023-06-22 20:17:50 +0700

    minggu-3

--- form_register.php
+++ form_register.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
-</head>
+</head> 
 
 <body style="font-family: 'Poppins', Courier, monospace;">
     <div class="container">
